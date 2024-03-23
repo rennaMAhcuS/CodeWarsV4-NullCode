bbf49e501a4a65fb083b7ff9f1b947e0663bd3c3
rennaMAhcuS <rennanahcus@git>
2024-03-23 17:25:01 +0530

    Script Working ;)

--- Temp.py
+++ Temp.py
@@ -114,40 +114,40 @@ def MoveTowards(x, y, Pirate):
         return (position[1] < y) * 2 + 1
 
 
-# def MoveAway(x, y, Pirate):
-#     position = Pirate.getPosition()
-#     if position[0] == x and position[1] == y:
-#         return random.randint(1, 4)
-#     if random.randint(1, 2) == 1:
-#         return (position[0] < x) * 2 + 2
-#     else:
-#         return (position[1] > y) * 2 + 1
+def MoveAway(x, y, Pirate):
+    position = Pirate.getPosition()
+    if position[0] == x and position[1] == y:
+        return random.randint(1, 4)
+    if random.randint(1, 2) == 1:
+        return (position[0] < x) * 2 + 2
+    else:
+        return (position[1] > y) * 2 + 1
 
 
-# def circleAround(x, y, radius, Pirate, initial="abc", clockwise=True):
-#     position = Pirate.getPosition()
-#     rx = position[0]
-#     ry = position[1]
-#     pos = [[x + i, y + radius] for i in range(-1 * radius, radius + 1)]
-#     pos.extend([[x + radius, y + i] for i in range(radius - 1, -1 * radius - 1, -1)])
-#     pos.extend([[x + i, y - radius] for i in range(radius - 1, -1 * radius - 1, -1)])
-#     pos.extend([[x - radius, y + i] for i in range(-1 * radius + 1, radius)])
-#     if [rx, ry] not in pos:
-#         if initial != "abc":
-#             return MoveTowards(initial[0], initial[1], Pirate)
-#         if rx in [x + i for i in range(-1 * radius, radius + 1)] and ry in [
-#             y + i for i in range(-1 * radius, radius + 1)
-#         ]:
-#             return MoveAway(x, y, Pirate)
-#         else:
-#             return MoveTowards(x, y, Pirate)
-#     else:
-#         index = pos.index([rx, ry])
-#         return MoveTowards(
-#             pos[(index + (clockwise * 2) - 1) % len(pos)][0],
-#             pos[(index + (clockwise * 2) - 1) % len(pos)][1],
-#             Pirate,
-#         )
+def circleAround(x, y, radius, Pirate, initial="abc", clockwise=True):
+    position = Pirate.getPosition()
+    rx = position[0]
+    ry = position[1]
+    pos = [[x + i, y + radius] for i in range(-1 * radius, radius + 1)]
+    pos.extend([[x + radius, y + i] for i in range(radius - 1, -1 * radius - 1, -1)])
+    pos.extend([[x + i, y - radius] for i in range(radius - 1, -1 * radius - 1, -1)])
+    pos.extend([[x - radius, y + i] for i in range(-1 * radius + 1, radius)])
+    if [rx, ry] not in pos:
+        if initial != "abc":
+            return MoveTowards(initial[0], initial[1], Pirate)
+        if rx in [x + i for i in range(-1 * radius, radius + 1)] and ry in [
+            y + i for i in range(-1 * radius, radius + 1)
+        ]:
+            return MoveAway(x, y, Pirate)
+        else:
+            return MoveTowards(x, y, Pirate)
+    else:
+        index = pos.index([rx, ry])
+        return MoveTowards(
+            pos[(index + (clockwise * 2) - 1) % len(pos)][0],
+            pos[(index + (clockwise * 2) - 1) % len(pos)][1],
+            Pirate,
+        )
 
 
 def OppDiagonalPoints(x1, y0, x0, y1):
@@ -246,16 +246,16 @@ def ActPirate(pirate):
     TeamSignalParsed = TeamSignal.split(".")
     FoundIslands = []
 
-    for i in TeamSignalParsed:
-        if i != "":
-            FoundIslands.append(str(i[0]))
-
     # Setting and Appending TeamSignal
     if IslandData[1] != "0" and IslandData[1] not in FoundIslands:
+        if TeamSignalParsed != [""]:
+            print(TeamSignalParsed)
+            for i in TeamSignalParsed:
+                if i != "":
+                    FoundIslands += str(i[0])
         NewSignal = pirate.getTeamSignal() + IslandData[1] + ":" + IslandData[0][0] + "," + IslandData[0][1] + "."
         pirate.setTeamSignal(NewSignal)
         FoundIslands.append(IslandData[1])
-        print(TeamSignalParsed)
 
     if int(pirate.getID()) % 11 in range(8):
         return Movement(pirate)
@@ -270,13 +270,9 @@ def ActPirate(pirate):
                         PirateIslandData += IslandCoordinates
                         break
 
-            if PirateIslandData != "":
-                PirateIslandCoordinateX = int(PirateIslandData.split(":")[1].split(",")[0])
-                PirateIslandCoordinateY = int(PirateIslandData.split(":")[1].split(",")[1])
+            PirateIslandCoordinateX = 0
+            PirateIslandCoordinateY = 0
 
-                return MoveTowards(PirateIslandCoordinateX, PirateIslandCoordinateY, pirate)
-            else:
-                return Movement(pirate)
         else:
             return Movement(pirate)
     pass
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import testing_scripts.script_opponent as opponent
 from random import randrange as rand
 
 if __name__ == "__main__":
-    dim = int(rand(10, 65, 2))
+    dim = int(rand(20, 65, 2))
     G = Game((dim, dim), opponent, script_development)
     # G = Game((20, 20), opponent, script_development)
     G.run_game()
