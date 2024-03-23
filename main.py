058dcfb1d11c4284193e147e907b90d586d07ed6
rennaMAhcuS <rennanahcus@git>
2024-03-23 10:28:53 +0530

    --

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from engine.main import Game
 # import script
-import script_development
-import sample_scripts.scriptred
+# import script_development
+import sample_scripts.scriptred as red
 import testing_scripts.script_opponent as opponent
 from random import randrange as rand
 
 if __name__ == "__main__":
     # dim = rand(2, 65, 2)
-    G = Game((40, 40), sample_scripts.scriptred, sample_scripts.scriptred)
+    G = Game((40, 40), red, opponent)
     G.run_game()
